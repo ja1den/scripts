d19940e2c44265700a72a7a8384e511479593a6d
Jadie-Wadie <jaidentdouglas@gmail.com>
2020-11-05 19:51:31 +1030

    refactor(image): update anchorage.ts

--- scripts/image/anchorage.ts
+++ scripts/image/anchorage.ts
@@ -13,43 +13,43 @@ const root = resolve(__dirname, '../../build/anchorage');
 if (!existsSync(root)) mkdirSync(root, { recursive: true });
 
 // Logo
-const i1 = createCanvas(1080, 1080);
-const c1 = i1.getContext('2d');
+const logo = createCanvas(1080, 1080);
+const cL = logo.getContext('2d');
 
-const w = i1.width * 0.5;
+const w = logo.width * 0.5;
 const h = w * Math.cos(Math.PI / 6);
 
-c1.translate(i1.width / 2, i1.height / 2);
+cL.translate(logo.width / 2, logo.height / 2);
 
-c1.beginPath();
+cL.beginPath();
 
-c1.moveTo(0 - w / 2, h / 2);
-c1.lineTo(w / 2, h / 2);
-c1.lineTo(0, 0 - h / 2);
+cL.moveTo(0 - w / 2, h / 2);
+cL.lineTo(w / 2, h / 2);
+cL.lineTo(0, 0 - h / 2);
 
-c1.closePath();
+cL.closePath();
 
-c1.strokeStyle = '#FFF';
-c1.lineWidth = 80;
+cL.strokeStyle = '#FFF';
+cL.lineWidth = 80;
 
-c1.stroke();
+cL.stroke();
 
-c1.clearRect(0 - w, h / 2 - 120, i1.width, 80);
+cL.clearRect(0 - w, h / 2 - 120, logo.width, 80);
 
-// Black
-const i2 = createCanvas(1080, 1080);
-const c2 = i2.getContext('2d');
+// Back
+const back = createCanvas(1080, 1080);
+const cB = back.getContext('2d');
 
-c2.fillStyle = '#111';
-c2.fillRect(0, 0, i2.width, i2.height);
+cB.fillStyle = '#111';
+cB.fillRect(0, 0, back.width, back.height);
 
-c2.drawImage(i1, 0, 0);
+cB.drawImage(logo, 0, 0);
 
 // Export
-i2.createPNGStream()
-	.pipe(createWriteStream(resolve(root, 'black.png')))
-	.on('finish', () => console.log('black.png'.cyan));
+logo.createPNGStream()
+	.pipe(createWriteStream(resolve(root, 'anchorage.png')))
+	.on('finish', () => console.log('anchorage.png'.cyan));
 
-i1.createPNGStream()
-	.pipe(createWriteStream(resolve(root, 'clear.png')))
-	.on('finish', () => console.log('clear.png'.cyan));
+back.createPNGStream()
+	.pipe(createWriteStream(resolve(root, 'anchorage-back.png')))
+	.on('finish', () => console.log('anchorage-back.png'.cyan));
