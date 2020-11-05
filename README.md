07f8cd0abc38f14ec3427ef7771d0d8a60ee234f
Jadie-Wadie <jaidentdouglas@gmail.com>
2020-11-05 18:50:16 +1030

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,46 @@
+# Scripts
+
+> A collection of useful scripts.
+
+## Introduction
+
+This is a collection of small things that aren't big enough for their own project.
+
+## Usage
+
+Everything in this repository can be run with [npm](https://www.npmjs.com/).
+
+```sh
+npm start
+```
+
+For ease of development, scripts can also be run through the CLI.
+
+```sh
+npm start <action> <script>
+```
+
+---
+
+Every script must begin with a shebang.
+
+| Language   | Shebang                  |
+| ---------- | ------------------------ |
+| JavaScript | `#!/usr/bin/env node`    |
+| TypeScript | `#!/usr/bin/env ts-node` |
+| Bash       | `#!/usr/bin/env bash`    |
+| Python     | `#!/usr/bin/env python3` |
+
+---
+
+You'll also need permission to execute the file (`744`).
+
+```sh
+npm run chmod
+```
+
+The `chmod` script will update script permissions recursively.
+
+## License
+
+[MIT](LICENSE)
---
+++ src/index.ts
@@ -0,0 +1,79 @@
+// Import
+import 'colors';
+
+import { readdirSync } from 'fs';
+import { resolve } from 'path';
+
+import parseArgs from 'minimist';
+import prompts from 'prompts';
+
+import { spawn } from 'child_process';
+
+// Choose Action
+async function chooseAction(directory: string) {
+	const actions = readdirSync(directory, {
+		withFileTypes: true
+	}).filter(dirent => dirent.isDirectory());
+
+	const { action }: { action: string } = await prompts({
+		type: 'select',
+		name: 'action',
+		message: ' What type of script would you like to run?',
+		choices: actions.map(action => ({
+			title: action.name,
+			value: action.name
+		}))
+	});
+
+	return action;
+}
+
+// Choose Script
+async function chooseScript(action: string) {
+	const scripts = readdirSync(resolve(__dirname, '../scripts', action), {
+		withFileTypes: true
+	}).filter(dirent => !dirent.isDirectory());
+
+	const { script }: { script: string } = await prompts({
+		type: 'select',
+		name: 'script',
+		message: ' Which script would you like to run?',
+		choices: scripts.map(script => ({
+			title: script.name,
+			value: script.name
+		}))
+	});
+
+	return script;
+}
+
+// Spawn Script
+async function spawnScript(script: string) {
+	const child = spawn(script);
+
+	child.stdout.on('data', data => process.stdout.write(data.toString()));
+	child.stderr.on('data', data => process.stderr.write(data.toString()));
+
+	child.on('error', err => console.error(err.message.red));
+}
+
+// Main
+async function main() {
+	const args = parseArgs(process.argv.slice(2));
+
+	let action: string;
+	let script: string;
+
+	if (args._.length == 2) {
+		action = args._[0];
+		script = args._[1];
+	} else {
+		action = await chooseAction(resolve(__dirname, '../scripts'));
+		script = await chooseScript(action);
+
+		console.log();
+	}
+
+	spawnScript(resolve(__dirname, '../scripts', action, script));
+}
+main();
