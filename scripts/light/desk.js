9f69dd7faf7abb08461a3b36a944c278ababa6f5
Jadie-Wadie <jaidentdouglas@gmail.com>
2020-11-11 15:46:53 +1030

    feat(light): update desk.js
    The script now toggles the current light state.

--- scripts/light/desk.js
+++ scripts/light/desk.js
@@ -12,19 +12,23 @@ async function main() {
 		mac: 'D0:73:D5:29:45:CB'
 	});
 
-	await desk.deviceSetPower({ level: 1 });
+	const { level } = await desk.deviceGetPower();
 
-	await desk.multiZoneSetColorZones({
-		start: 0,
-		end: 7,
-		color: { css: '#7D0080', brightness: 0.25 }
-	});
+	if (level === 0) {
+		await desk.multiZoneSetColorZones({
+			start: 0,
+			end: 7,
+			color: { css: '#7D0080', brightness: 0.25 }
+		});
 
-	await desk.multiZoneSetColorZones({
-		start: 8,
-		end: 15,
-		color: { css: '#33FF00', brightness: 0.45 }
-	});
+		await desk.multiZoneSetColorZones({
+			start: 8,
+			end: 15,
+			color: { css: '#33FF00', brightness: 0.45 }
+		});
+	}
+
+	await desk.deviceSetPower({ level: 1 - level });
 
 	console.log('#7D0080'.magenta + ' - ' + '#33FF00'.green);
 
